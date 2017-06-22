9ba1ff518fabcaefd2137fc26d4711ebff6f6df7
Linhvh <linhvh112@gmail.com>
2017-06-22 14:32:03 +0700

    Add missing file for commit "Add method to calculate Tagname = Sum of Tagnames"

---
+++ ScadaHisAPI/Domain/SumTagName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ScadaHisAPI
+{
+    public class SumTagName
+    {
+        public string TagName { get; set; }
+
+        public List<string> SumList { get; set; }
+
+        public SumTagName(string tagname)
+        {
+            this.TagName = tagname;
+            SumList = null;
+        }
+
+    }
+}
